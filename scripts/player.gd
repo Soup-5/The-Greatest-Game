5d30fbdf01dafbf1ef8eea3c2b0feae9ed45afac
Soup-5 <93955560+Soup-5@users.noreply.github.com>
2024-02-29 08:33:23 -0800

    add player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,7 +9,9 @@ var dir: Vector2
 
 func _physics_process(delta: float) -> void:
 	velocity.y += -gravity * delta
-	dir = Input.get_vector('move_backward', 'move_forward', 'move_left', 'move_right')
+	dir = Input.get_vector('move_left', 'move_right', 'move_forward', 'move_backward')
+	if Input.is_action_just_pressed('jump') and is_on_floor():
+		velocity.y = jump_force
 	velocity.x = dir.x * move_speed
 	velocity.z = dir.y * move_speed
 	move_and_slide()
