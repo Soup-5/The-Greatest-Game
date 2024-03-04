6f9627cbadab3b749aa44b5e7a1a4e1c7ffbd549
Soup-5 <93955560+Soup-5@users.noreply.github.com>
2024-03-04 09:13:24 -0800

    add pause menu

--- scripts/debug.gd
+++ scripts/debug.gd
@@ -4,5 +4,5 @@ extends MarginContainer
 @onready var label := $Stats
 
 func _process(_delta: float) -> void:
-	#label.text = 'velocity: '+(str(player.velocity))+'\ndirection: '+(str(player.dir))+'\nfps: '+str(Engine.get_frames_per_second())
+	label.text = 'velocity: '+(str(player.velocity))+'\ndirection: '+(str(player.dir))+'\nfps: '+str(Engine.get_frames_per_second())
 	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,18 +6,19 @@ extends CharacterBody3D
 @export var jump_force: float = 5.0
 @export var mouse_sense: float = 1.0
 
-var dir: Vector2
+var dir: Vector3
 
 func _enter_tree() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
 	velocity.y += -gravity * delta
-	dir = Input.get_vector('move_left', 'move_right', 'move_forward', 'move_backward')
+	var input := Input.get_vector('move_left', 'move_right', 'move_forward', 'move_backward')
+	dir = transform.basis * Vector3(input.x, 0, input.y)
 	if Input.is_action_just_pressed('jump') and is_on_floor():
 		velocity.y = jump_force
 	velocity.x = dir.x * move_speed
-	velocity.z = dir.y * move_speed
+	velocity.z = dir.z * move_speed
 	move_and_slide()
 	
 func _unhandled_input(event: InputEvent) -> void:
